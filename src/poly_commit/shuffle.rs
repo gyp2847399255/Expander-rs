c14fafdd1b53719604cde3f77f41b39ce90aef31
Yanpei Guo <2847399255@qq.com>
2024-07-28 22:18:19 +0800

    1

--- src/poly_commit/shuffle.rs
+++ src/poly_commit/shuffle.rs
@@ -7,8 +7,8 @@ pub struct ShufflePcProver<F: Field + FieldSerde> {
 }
 
 #[derive(Debug, Default)]
-struct  ShufflePcProof<F: Field + FieldSerde> {
-    ex_eval: Vec<F>
+struct ShufflePcProof<F: Field + FieldSerde> {
+    ex_eval: Vec<F>,
 }
 
 impl<F: Field + FieldSerde> PolyCommitProver<F> for ShufflePcProver<F> {
@@ -16,5 +16,20 @@ impl<F: Field + FieldSerde> PolyCommitProver<F> for ShufflePcProver<F> {
     type Commitment = RawCommitment<F>;
     type Proof = ShufflePcProof<F>;
 
-    
+    fn new(pp: Self::Param, poly: &MultiLinearPoly<F>) -> Self {
+        ShufflePcProver { poly: poly.clone() }
+    }
+
+    fn commit(&self) -> Self::Commitment {
+        RawCommitment {
+            poly_vals: self.poly.evals.clone(),
+        }
+    }
+
+    fn open(&self, point: &[<F as Field>::BaseField], transcript: &mut crate::Transcript) -> (F, Self::Proof) {
+        (
+            MultiLinearPoly::eval_multilinear(&self.poly.evals, point),
+            (),
+        )
+    }
 }
