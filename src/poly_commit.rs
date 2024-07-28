e90ec1c0146fb3dc6995a100b39e96b05d3d8731
Yanpei Guo <2847399255@qq.com>
2024-07-28 17:07:55 +0800

    1

--- src/poly_commit.rs
+++ src/poly_commit.rs
@@ -1,4 +1,29 @@
 pub mod raw;
+use std::fmt::Debug;
+
+use arith::{Field, MultiLinearPoly};
+
+use crate::Transcript;
+
 pub use self::raw::*;
 
-pub trait PolynomialCommitment {}
+pub trait CommitmentSerde {}
+
+pub trait PolyCommitProver<F: Field> {
+    type Param: Clone;
+    type Commitment: Clone + Debug + Default + CommitmentSerde;
+    type Proof;
+
+    fn new(pp: Self::Param, poly: &MultiLinearPoly<F>, transcript: &mut Transcript) -> Self;
+    fn commit(&self) -> Self::Commitment;
+    fn open(&self, point: Vec<F>, transcript: &mut Transcript) -> Self::Proof;
+}
+
+pub trait PolyCommitVerifier<F: Field> {
+    type Param: Clone;
+    type Commitment: Clone + Debug + Default + CommitmentSerde;
+    type Proof;
+
+    fn new(pp: Self::Param, commit: Self::Commitment) -> Self;
+    fn verify(&self, proof: Self::Proof) -> bool;
+}
